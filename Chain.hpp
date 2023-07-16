e9fc0a13ccbb156a8eb31be85e63e45b0d7da401
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-07-16 22:04:28 +0700

    Update Chain.hpp
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- Chain.hpp
+++ Chain.hpp
@@ -42,39 +42,73 @@ namespace SPHINX_Chain {
     class Chain {
     public:
         Chain();
+
         void addBlock(const SPHINXBlock::Block& block);
+
+        bool isChainValid() const;
+
         std::string getBlockHash(uint32_t blockHeight) const;
+
         void transferFromSidechain(const SPHINX_Chain::Chain& sidechain, const std::string& blockHash);
+
         void handleBridgeTransaction(const std::string& bridge, const std::string& targetChain, const std::string& transaction);
-        nlohmann::json toJson() const;
-        void fromJson(const nlohmann::json& chainJson);
-        bool save(const nlohmann::json& chainJson, const std::string& filename) const;
+
+        json toJson() const;
+
+        void fromJson(const json& chainJson);
+
+        bool save(const json& chainJson, const std::string& filename) const;
+
         static Chain load(const std::string& filename);
+
         SPHINXBlock::Block getGenesisBlock() const;
+
         SPHINXBlock::Block getBlockAt(size_t index) const;
+
         size_t getChainLength() const;
+
         void visualizeChain() const;
+
         void connectToSidechain(const Chain& sidechain);
+
         void transferFromSidechain(const std::string& sidechainAddress, const std::string& senderAddress, double amount);
+
         void createBlockchainBridge(const Chain& targetChain);
+
         void handleBridgeTransaction(const std::string& bridgeAddress, const std::string& recipientAddress, double amount);
+
         void performAtomicSwap(const Chain& targetChain, const std::string& senderAddress, const std::string& receiverAddress, double amount);
+
         void signTransaction(SPHINXTrx::Transaction& transaction);
+
         void broadcastTransaction(const SPHINXTrx::Transaction& transaction);
+
         void updateBalance(const std::string& address, double amount);
+
         double getBalance(const std::string& address) const;
+
         bool verifyAtomicSwap(const SPHINXTrx::Transaction& transaction, const Chain& targetChain) const;
+
         void handleTransfer(const SPHINXTrx::Transaction& transaction);
+
         std::string getBridgeAddress() const;
+
         std::string getBridgeSecret() const;
-        // Sharding chain functionality
+
         void createShard(const std::string& shardName);
+
         void joinShard(const std::string& shardName, const Chain& shardChain);
+
         void transferToShard(const std::string& shardName, const std::string& senderAddress, const std::string& recipientAddress, double amount);
+
         void handleShardTransfer(const std::string& shardName, const SPHINXTrx::Transaction& transaction);
+
         void handleShardBridgeTransaction(const std::string& shardName, const std::string& bridgeAddress, const std::string& recipientAddress, double amount);
+
         void performShardAtomicSwap(const std::string& shardName, const Chain& targetShard, const std::string& senderAddress, const std::string& receiverAddress, double amount);
+
         void updateShardBalance(const std::string& shardName, const std::string& address, double amount);
+
         double getShardBalance(const std::string& shardName, const std::string& address) const;
 
     private:
@@ -84,8 +118,10 @@ namespace SPHINX_Chain {
             std::string bridgeSecret;
             std::unordered_map<std::string, double> balances;
         };
+
         std::vector<Shard> shards_;
         std::vector<SPHINXBlock::Block> blocks_;
+        std::string privateKey_;
         std::string publicKey_;
         static constexpr uint32_t BLOCK_NOT_FOUND = std::numeric_limits<uint32_t>::max();
         std::unordered_map<std::string, uint32_t> shardIndices_;
@@ -93,19 +129,19 @@ namespace SPHINX_Chain {
         std::unordered_map<std::string, double> balances_;
         std::string bridgeAddress_;
         std::string bridgeSecret_;
-        SPHINX_Chain::Chain targetChain_;
+        Chain targetChain_;
     };
 
     Chain::Chain() {
         // Create the genesis block with the provided message
         std::string genesisMessage = "Welcome to Post-Quantum era, The Beginning of a Secured-Trustless Network will start from here - SPHINX Network";
-        SPHINXBlock genesisBlock(genesisMessage);
+        SPHINXBlock::Block genesisBlock(genesisMessage);
 
         // Add the genesis block to the chain
         blocks_.push_back(genesisBlock);
     }
 
-    void Chain::addBlock(const SPHINXBlock& block) {
+    void Chain::addBlock(const SPHINXBlock::Block& block) {
         // Add block to the blockchain
         // Calculate the hash of the block's data
         std::string blockHash = block.calculateBlockHash();
@@ -120,8 +156,8 @@ namespace SPHINX_Chain {
     bool Chain::isChainValid() const {
         // Validate the integrity of the blockchain
         for (size_t i = 1; i < blocks_.size(); ++i) {
-            const SPHINXBlock& currentBlock = blocks_[i];
-            const SPHINXBlock& previousBlock = blocks_[i - 1];
+            const SPHINXBlock::Block& currentBlock = blocks_[i];
+            const SPHINXBlock::Block& previousBlock = blocks_[i - 1];
 
             // Verify the block's hash and previous block hash
             if (currentBlock.getBlockHash() != currentBlock.calculateBlockHash() ||
@@ -138,12 +174,12 @@ namespace SPHINX_Chain {
         return true;
     }
 
-    SPHINXBlock Chain::getGenesisBlock() const {
+    SPHINXBlock::Block Chain::getGenesisBlock() const {
         // Get the first block in the blockchain
         return blocks_.front();
     }
 
-    SPHINXBlock Chain::getBlockAt(size_t index) const {
+    SPHINXBlock::Block Chain::getBlockAt(size_t index) const {
         // Get the block at the specified index
         if (index < blocks_.size()) {
             return blocks_[index];
@@ -160,27 +196,27 @@ namespace SPHINX_Chain {
     void Chain::visualizeChain() const {
         // Visualize the blockchain for analysis or presentation purposes
         for (size_t i = 0; i < blocks_.size(); ++i) {
-            const SPHINXBlock& block = blocks_[i];
+            const SPHINXBlock::Block& block = blocks_[i];
             std::cout << "Block " << i << " - Hash: " << block.getBlockHash() << std::endl;
             // Print or display other block details as desired
         }
     }
 
-    nlohmann::json Chain::toJson() const {
+    json Chain::toJson() const {
         json chainJson;
-        chainJson["blocks"] = nlohmann::json::array();
-        for (const SPHINXBlock& block : blocks_) {
+        chainJson["blocks"] = json::array();
+        for (const SPHINXBlock::Block& block : blocks_) {
             chainJson["blocks"].push_back(block.toJson());
         }
         return chainJson;
     }
 
-    void Chain::fromJson(const nlohmann::json& chainJson) {
+    void Chain::fromJson(const json& chainJson) {
         blocks_.clear();
         if (chainJson.contains("blocks") && chainJson["blocks"].is_array()) {
             const json& blocksJson = chainJson["blocks"];
             for (const json& blockJson : blocksJson) {
-                SPHINXBlock block;
+                SPHINXBlock::Block block;
                 block.fromJson(blockJson);
                 blocks_.push_back(block);
             }
